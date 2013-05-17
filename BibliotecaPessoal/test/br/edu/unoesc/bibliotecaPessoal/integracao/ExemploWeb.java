6e04c4c5447aab6cc0c72631216bc6359d8f0790
andre.forchesatto <andreforchesatto@gmail.com>
2013-05-17 16:56:51 -0300

    adicionado vraptor convertido o projeto para web para poder utilizar testes de integração com selenium

---
+++ BibliotecaPessoal/test/br/edu/unoesc/bibliotecaPessoal/integracao/ExemploWeb.java
@@ -0,0 +1,22 @@
+package br.edu.unoesc.bibliotecaPessoal.integracao;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class ExemploWeb {
+
+	public static void main(String[] args){
+		WebDriver driver = new FirefoxDriver();
+		
+		driver.get("http://www.google.com.br/");
+		
+		WebElement campoDeTexto = driver.findElement(By.name("q"));
+		campoDeTexto.sendKeys("Unoesc");
+		
+		campoDeTexto.submit();
+		
+		driver.getPageSource().contains("Unoesc");
+	}
+}
---
+++ BibliotecaPessoal/test/br/edu/unoesc/bibliotecaPessoal/integracao/MaterialWeb.java
@@ -0,0 +1,5 @@
+package br.edu.unoesc.bibliotecaPessoal.integracao;
+
+public class MaterialWeb {
+
+}
